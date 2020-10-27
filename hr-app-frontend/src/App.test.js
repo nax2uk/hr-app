1d9b6011b47fc8f700b6af622b2568e8075f0d5a
Richard Pentecost <richard.pentecost@madetech.com>
2020-10-27 16:22:20 +0000

    react-app and testing suite setup

---
+++ hr-app-frontend/src/App.test.js
@@ -0,0 +1,13 @@
+// import { render, screen } from '@testing-library/react';
+import React from 'react';
+import Enzyme from 'enzyme';
+import App from './App';
+
+describe('App', () => {
+  it('renders a div with the className = "app"', () => {
+    const wrapper = Enzyme.shallow((
+      <App />
+    ));
+    expect(wrapper.find('div').hasClass('app')).toBe(true)
+  });
+})
