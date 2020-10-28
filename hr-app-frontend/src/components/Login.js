e7bc7b5ee2086f8d2fdd9772cb15935bc93eca41
Richard Pentecost <richard.pentecost@madetech.com>
2020-10-28 15:22:58 +0000

    Login css file added

--- hr-app-frontend/src/components/Login.js
+++ hr-app-frontend/src/components/Login.js
@@ -1,8 +1,20 @@
 import React from 'react'
+import '../style/Login.css';
 
 const Login = () => {
     return (
-        <h1>Login Page</h1>
+        <>
+            <h1>Login Page</h1>
+            <div className='loginContainer'>
+                <form className='loginForm'>
+                    <label>Username</label>
+                    <input />
+                    <label>Password</label>
+                    <input />
+                    <button>Login</button>
+                </form>
+            </div>
+        </>
     )
 }
 
