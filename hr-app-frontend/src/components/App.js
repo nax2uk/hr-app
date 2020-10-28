e5fab4752c297737629049098bbdf107f8fd119e
Richard Pentecost <richard.pentecost@madetech.com>
2020-10-28 12:11:18 +0000

    Login file created

--- hr-app-frontend/src/components/App.js
+++ hr-app-frontend/src/components/App.js
@@ -1,10 +1,10 @@
 import React from 'react';
-import './App.css';
+import '../style/App.css';
 
 function App() {
   return (
     <div className="app">
-      HR-App
+      HR App
     </div>
   );
 }
